004750aa0b2970fead0e52a9f4ade1a9aa8d916a
HAlGhanim <118439866+HAlGhanim@users.noreply.github.com>
2023-10-18 16:56:51 +0300

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const { model, Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
+  phoneNumber: { type: Number, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+module.exports = model("User", UserSchema);
